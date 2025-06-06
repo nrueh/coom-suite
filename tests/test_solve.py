005724c34420129c89052debc301a1480a44108e
Nicolas Rühling <nruehling@uni-potsdam.de>
2025-06-06 17:01:44 +0200

    added constraint handler dependency

--- tests/test_solve.py
+++ tests/test_solve.py
@@ -121,7 +121,7 @@ class TestClingo(TestCase):
 
     def test_user_input(self) -> None:
         """
-        Test solving user input
+        Test solving user input (clingo).
         """
         self.run_test("add_part")
         self.run_test("add_attribute")
@@ -249,7 +249,7 @@ class TestFclingo(TestCase):
 
     def test_user_input(self) -> None:
         """
-        Test solving user input
+        Test solving user input (fclingo).
         """
         self.run_test("add_part")
         self.run_test("add_attribute")
@@ -261,3 +261,130 @@ class TestFclingo(TestCase):
         self.run_test("set_invalid_type")
         self.run_test("set_invalid_value_discrete")
         # self.run_test("set_invalid_value_num")
+
+
+class TestConstraintHandler(TestCase):
+    """
+    Test cases for the constraint handler encoding.
+    """
+
+    def run_test(self, test_name: str) -> None:
+        """
+        Runs a clintest test with the clingo encoding.
+        """
+        test, program, files = unpack_test(test_name, TESTS_SOLVE)
+        run_test(test, files=files, program=program, ctl_args=["0"], solver="constraint-handler")
+
+    def test_structure(self) -> None:
+        """
+        Test structure generation (constraint handler).
+        """
+        self.run_test("empty")
+        self.run_test("optional_part")
+        self.run_test("mandatory_part")
+        self.run_test("part_with_cardinality")
+        self.run_test("optional_part_with_subpart")
+
+    def test_attributes(self) -> None:
+        """
+        Test attribute generation (constraint handler).
+        """
+        self.run_test("simple_discrete")
+        self.run_test("optional_discrete")
+        self.run_test("multiple_discrete")
+
+        self.run_test("simple_integer")
+        self.run_test("optional_integer")
+        self.run_test("multiple_integer")
+
+    def test_boolean_constraints(self) -> None:
+        """
+        Test Boolean constraints (constraint handler).
+        """
+        self.run_test("eq_sat")
+        self.run_test("neq_sat")
+        self.run_test("le_sat")
+        self.run_test("leq_sat")
+        self.run_test("ge_sat")
+        self.run_test("geq_sat")
+
+        self.run_test("eq_unsat")
+        self.run_test("neq_unsat")
+        self.run_test("le_unsat")
+        self.run_test("leq_unsat")
+        self.run_test("ge_unsat")
+        self.run_test("geq_unsat")
+
+        self.run_test("par_sat")
+        self.run_test("neg_sat")
+        self.run_test("or_sat")
+        self.run_test("and_sat")
+
+        self.run_test("par_unsat")
+        self.run_test("neg_unsat")
+        self.run_test("or_unsat")
+        self.run_test("and_unsat")
+
+        self.run_test("binary_undef")
+        self.run_test("unary_undef")
+
+    def test_table_constraints(self) -> None:
+        """
+        Test table constraints (constraint handler).
+        """
+        self.run_test("table_discrete")
+        self.run_test("table_wildcard")
+        self.run_test("table_integer")
+        self.run_test("table_mixed")
+        self.run_test("table_undef")
+        self.run_test("table_undef2")
+        self.run_test("empty_table")
+
+    def test_arithmetics(self) -> None:
+        """
+        Test arithmetic formulas (constraint handler).
+        """
+        self.run_test("plus_sat")
+        self.run_test("minus_sat")
+        self.run_test("mult_sat")
+        self.run_test("unary_plus_sat")
+        self.run_test("unary_minus_sat")
+
+        self.run_test("plus_unsat")
+        self.run_test("minus_unsat")
+        self.run_test("mult_unsat")
+        self.run_test("unary_minus_unsat")
+
+        self.run_test("plus_undef_sat")
+        self.run_test("minus_undef_sat")
+        self.run_test("plus_undef_unsat")
+        self.run_test("minus_undef_unsat")
+
+        self.run_test("precedence_sat")
+        self.run_test("precedence_par_sat")
+        self.run_test("precedence_unsat")
+        self.run_test("precedence_par_unsat")
+
+    def test_aggregates(self) -> None:
+        """
+        Test aggregation functions (constraint handler).
+        """
+        self.run_test("count")
+        self.run_test("sum")
+        self.run_test("min")
+        self.run_test("max")
+
+    def test_user_input(self) -> None:
+        """
+        Test solving user input (constraint handler).
+        """
+        self.run_test("add_part")
+        self.run_test("add_attribute")
+        self.run_test("set_value_discrete")
+        self.run_test("set_value_integer")
+
+        self.run_test("set_invalid_variable")
+        self.run_test("add_invalid_variable")
+        self.run_test("set_invalid_type")
+        self.run_test("set_invalid_value_discrete")
+        self.run_test("set_invalid_value_num")
